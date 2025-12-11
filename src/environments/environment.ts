6e074b5b0165954329174e049ad548fd35bf0394
carlitospro2003 <carloseduardorsaucedo2003@gmail.com>
2025-12-10 19:25:54 -0600

    1.3.6 emoji

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://127.0.0.1:8000/api',
   appName: 'Lambda Fitness',
-  version: '1.3.5',
+  version: '1.3.6',
   recaptchaSiteKey: '6LcgbCUsAAAAABvBxNT4pRRYdd70_gkmmTXpmu2z', // Reemplazar con tu Site Key de Google reCAPTCHA v3
   firebase: {
     apiKey: "AIzaSyDPQ3HcafZsG9MJgwTYDM-oV-uCOBCIlnM",
